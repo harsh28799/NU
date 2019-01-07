573f7f34fcf6f1b5b5e2993c9cd5dacf29239e63
harsh28799 <302harsh@gmail.com>
2019-01-07 19:32:42 +0530

    Dummy Website

---
+++ home.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+      <title>NITRUtsav 2K19</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="index.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+      <style>
+            body{
+              background-color: black;
+            }
+            h2,h3,h4,h5,h6,p{
+              color: white;
+            }
+            body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+            .w3-bar-block .w3-bar-item {padding:20px}
+      </style>
+<body>
+
+
+<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button">Close Menu</a>
+  <a href="http://localhost/NU/signup.php" onclick="w3_close()" class="w3-bar-item w3-button">Signup</a>
+  <a href="http://localhost/NU/signin.php" onclick="w3_close()" class="w3-bar-item w3-button">Signin</a>
+  <a href="http://localhost/NU/registration.php" onclick="w3_close()" class="w3-bar-item w3-button">Campus Ambassador</a>
+  <a href="#events" onclick="w3_close()" class="w3-bar-item w3-button">Events</a>
+  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
+</nav>
+
+
+<div class="w3-top">
+  <div class="w3-white w3-xlarge" style="max-width:1200px;margin:auto">
+    <div class="w3-button w3-padding-8 w3-left" onclick="w3_open()">☰</div>
+    <div class="w3-center w3-padding-8"><h1>NITRUtsav 2K19</h1></div>
+  </div>
+</div>
+
+<br><br><br><br>
+
+<div class="w3-center"><h3>EVENTS</h3></div>
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px"><hr></div>
+<!-- <Image Grid> -->
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px" id="events">
+
+  <div class="w3-row-padding w3-padding-8 w3-center">
+    <div class="w3-quarter">
+      <img src="img/first.jpg" alt="FIRST" style="width:100%">
+      <h3>Skilya</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/second.jpg" alt="SECOND" style="width:100%">
+      <h3>Aps</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/third.jpg" alt="THIRD" style="width:100%">
+      <h3>Seaford</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/fourth.jpg" alt="FOURTH" style="width:100%">
+      <h3>Star Tuan</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+  </div>
+  
+  <div class="w3-row-padding w3-padding-8 w3-center">
+    <div class="w3-quarter">
+      <img src="img/fifth.jpg" alt="Popsicle" style="width:100%">
+      <h3>Hyenafox</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/sixth.jpg" alt="Salmon" style="width:100%">
+      <h3>Hyenafox</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/seventh.jpg" alt="Sandwich" style="width:100%">
+      <h3>Star Tuan</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/eighth.jpg" alt="Croissant" style="width:100%">
+      <h3>Thanh Tuan</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+  </div>
+
+  <div class="w3-row-padding w3-padding-8 w3-center">
+    <div class="w3-quarter">
+      <img src="img/ninth.jpg" alt="Popsicle" style="width:100%">
+      <h3>Aps</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/tenth.jpg" alt="Salmon" style="width:100%">
+      <h3>Skilya</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/eleventh.jpg" alt="Sandwich" style="width:100%">
+      <h3>Seaford</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/twelveth.jpg" alt="Croissant" style="width:100%">
+      <h3>Hyenafox</h3>
+      <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+    </div>
+  </div>
+</div>
+
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px"><hr id="about"></div>
+  <!-- <Footer> -->
+  <div class="w3-container w3-padding-32 w3-center">  
+    <div class="w3-center"><h3>ABOUT NITRUTSAV 2K19</h3><br></div>
+    <div class="w3-padding-32">
+      <h4><b>THE LARGEST CULTURAL FEST OF ODISHA</b></h4>
+      <h6><i>NITRutsav, the official cultural festival of NIT Rourkela, brings to you the golden ticket to travel back in time and relive the days of flamboyant hairdos and classy suspenders. ‘Cause, what could be better than giving the expression of the cultural diversity of NIT Rourkela, a retro twist?</i></h6>
+      <h3><b>This February, it’s all about “CARNIVAL”!</b></h3>
+      <p>Come, get on board with us for a 3-day long cultural extravaganza, with fun events, stellar exhibitions, engaging workshops and a star-studded celebrity night. Lose yourself to the festivities of this vintage affair, as NU’19 paints the city of Rourkela in myriad hues of black, white and retro!</p>
+    </div>
+  </div>
+
+<div class="w3-container w3-padding-8 w3-center">  
+    <h3>Desined by <a href="https://www.google.co.in/search?q=harsh+mohan&oq=harsh+mohan&aqs=chrome..69i57j0l5.2179j0j7&sourceid=chrome&ie=UTF-8"> HARSH MOHAN</a></h3>
+    <br>
+</div>
+
+<script>
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+</body>
+</html>
